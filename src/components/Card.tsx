f6e7c95cd51f0971238ce12052563a81ec008baf
vsv-noon <forvsn@gmail.com>
2026-05-02 06:38:57 +0700

    feat: add description to Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,6 +3,8 @@ import { Component } from 'react';
 type Character = {
   name: string;
   image: string;
+  status: string;
+  species: string;
 };
 
 type Props = {
@@ -17,6 +19,8 @@ export class Card extends Component<Props> {
       <div>
         <img src={character.image} alt={character.name} />
         <h2>{this.props.name}</h2>
+        <p>Status: {character.status}</p>
+        <p>Species: {character.species}</p>
       </div>
     );
   }
